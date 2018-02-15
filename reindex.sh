e32d7049a3b453d48cce817aa6bf42ee8f99375f
Abhimanyu Banerjee <manyu@sh-101-58.int>
2018-02-14 23:39:06 -0800

    Reindex the bams with removed organelles

---
+++ reindex.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+/oak/stanford/groups/akundaje/marinovg/programs/samtools-0.1.19/samtools index control_none.beads_col_v3a-GSM1924998.50mers.TAIR10.unique.removed_organelles.bam
+
+echo "Finished control 1"
+
+/oak/stanford/groups/akundaje/marinovg/programs/samtools-0.1.19/samtools index control_tnt-pixhalo_colamp_v3a-GSM1924999.50mers.TAIR10.unique.removed_organelles.bam
+
+echo "Finished control 2"
+
+
+
