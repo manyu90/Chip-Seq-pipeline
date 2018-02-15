f9fe8dca9ee96914b1ae387e2673386696024f06
Abhimanyu Banerjee <manyu@sh-101-58.int>
2018-02-15 00:21:48 -0800

    Cross correlation step

---
+++ Rscript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+Rscript /oak/stanford/groups/akundaje/marinovg/code/spp/spp_package/run_spp.R -c= control_none.beads_col_v3a-GSM1924998.50mers.TAIR10.unique.removed_organelles.bam.bai -p=8 -savp -rf -s=-0:2:400 -out= control_none.beads_col_v3a-GSM1924998.50mers.TAIR10.unique.removed_organelles.QC
+
+echo "Finished control1 "
+
+Rscript /oak/stanford/groups/akundaje/marinovg/code/spp/spp_package/run_spp.R -c= control_tnt-pixhalo_colamp_v3a-GSM1924999.50mers.TAIR10.unique.removed_organelles.bam.bai -p=8 -savp -rf -s=-0:2:400 -out= control_tnt-pixhalo_colamp_v3a-GSM1924999.50mers.TAIR10.unique.removed_organelles.QC
+
+echo "Finished control2" 
+
+
