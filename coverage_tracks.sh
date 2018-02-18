8967dac4a7e48d20652d43707a03211d23b98aa4
Abhimanyu Banerjee <manyu@sh-101-58.int>
2018-02-18 15:45:05 -0800

    Adding coverage tracks

---
+++ coverage_tracks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python /oak/stanford/groups/akundaje/marinovg/code/makewigglefromBAM-NH.py ==== control_none.beads_col_v3a-GSM1924998.50mers.TAIR10.unique.removed_organelles.bam   /oak/stanford/groups/akundaje/marinovg/genomes/TAIR10/TAIR10.chrom.sizes control_none.beads_col_v3a-GSM1924998.50mers.TAIR10.unique.removed_organelles.wig -notitle -uniqueBAM -RPM
+
+echo "Finished control1"
+
+python /oak/stanford/groups/akundaje/marinovg/code/makewigglefromBAM-NH.py ==== control_tnt-pixhalo_colamp_v3a-GSM1924999.50mers.TAIR10.unique.removed_organelles.bam   /oak/stanford/groups/akundaje/marinovg/genomes/TAIR10/TAIR10.chrom.sizes control_tnt-pixhalo_colamp_v3a-GSM1924999.50mers.TAIR10.unique.removed_organelles.wig -notitle -uniqueBAM -RPM 
+
+echo "Finished control2"
+
+
+
